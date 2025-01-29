7b7f16fb081390a7eb0e74f33222db69e4839aee
Luciano Palacio <luciano.palacio.dev@gmail.com>
2025-01-29 01:35:56 -0300

    feat: add cors to allow the local host origin

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
